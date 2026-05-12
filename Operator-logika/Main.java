a3d867e4b2a7b9660a62436f73f4fb66058d4d6f
Kenedy Nopriansyah <kenedynopriansyah@Kenedys-MacBook-Air.local>
2026-05-12 09:32:59 +0700

    for-each

--- Operator-logika/Main.java
+++ Operator-logika/Main.java
@@ -1,49 +1,49 @@
 public class Main {
-    public static void main(String[] args) {
-        // AND, OR, XOR, NOT
+  public static void main(String[] args) {
+    // AND, OR, XOR, NOT
 
-        Boolean a = true;
-        Boolean b = false;
+    Boolean a = true;
+    Boolean b = false;
 
-        // ini adalah operator OR
-        System.out.printf("%b OR %b = %b\n", a, b, (a || b));
+    // ini adalah operator OR
+    System.out.printf("%b OR %b = %b\n", a, b, (a || b));
 
-        b = true;
-        System.out.printf("%b OR %b = %b\n", a, b, (a || b));
+    b = true;
+    System.out.printf("%b OR %b = %b\n", a, b, (a || b));
 
-        a = false;
-        b = false;
-        System.out.printf("%b OR %b = %b\n", a, b, (a || b));
+    a = false;
+    b = false;
+    System.out.printf("%b OR %b = %b\n", a, b, (a || b));
 
-        // ini adalah operator AND
-        System.out.printf("%b AND %b = %b\n", a, b, (a && b));
+    // ini adalah operator AND
+    System.out.printf("%b AND %b = %b\n", a, b, (a && b));
 
-        b = true;
-        System.out.printf("%b AND %b = %b\n", a, b, (a && b));
+    b = true;
+    System.out.printf("%b AND %b = %b\n", a, b, (a && b));
 
-        a = false;
-        b = false;
-        System.out.printf("%b AND %b = %b\n", a, b, (a && b));
+    a = false;
+    b = false;
+    System.out.printf("%b AND %b = %b\n", a, b, (a && b));
 
-        a = true;
-        b = true;
-        System.out.printf("%b AND %b = %b\n", a, b, (a && b));
+    a = true;
+    b = true;
+    System.out.printf("%b AND %b = %b\n", a, b, (a && b));
 
-        // ini adalah operator XOR (^)
-        b = false;
-        System.out.printf("%b ^ %b = %b\n", a, b, (a ^ b));
+    // ini adalah operator XOR (^)
+    b = false;
+    System.out.printf("%b ^ %b = %b\n", a, b, (a ^ b));
 
-        b = true;
-        System.out.printf("%b ^ %b = %b\n", a, b, (a ^ b));
+    b = true;
+    System.out.printf("%b ^ %b = %b\n", a, b, (a ^ b));
 
-        a = false;
-        b = false;
-        System.out.printf("%b ^ %b = %b\n", a, b, (a ^ b));
+    a = false;
+    b = false;
+    System.out.printf("%b ^ %b = %b\n", a, b, (a ^ b));
 
-        a = true;
-        System.out.printf("NOT %b\n", !a);
+    a = true;
+    System.out.printf("NOT %b\n", !a);
 
-        a = false;
-        System.out.printf("NOT %b\n", !a);
-    }    
+    a = false;
+    System.out.printf("NOT %b\n", !a);
+  }
 }
--- for-each/Main.java
+++ for-each/Main.java
@@ -1,16 +1,31 @@
 public class Main {
-    public static void main(String[] args) {
-        // Array
-        String[] strings = {"Ayam", "Kucing", "Ikan", "Bebek"};
+  public static void main(String[] args) {
+    int[] n = { 1, 2, 3, 4, 5, 6 };
+    float[] f = { 1.2f, 2.3f, 3.4f, 4.4f, 5.5f };
+    double[] d = { 1d, 2d, 3d, 4d, 5d };
 
-        // For Loop, ketika ingin mengeluarkan data harus menggunakan index
-        for(int i =0; i < strings.length; i++) {
-            System.out.println(strings[i]);
-        }
+    for (int i = 0; i < n.length; i++) {
+      System.out.printf("Nilai dari integer forloop, index: %d, value: %d \n", i, n[i]);
+    }
 
-        // For Each
-        for(String value: strings) {
-            System.out.println("Nama hewan adalah : " + value);
-        }
-    }    
+    for (int i = 0; i < f.length; i++) {
+      System.out.printf("Nilai dari float forloop, index: %d, value: %f \n", i, f[i]);
+    }
+
+    for (int i = 0; i < d.length; i++) {
+      System.out.printf("Nilai dari double forloop, index: %d, value: %f \n", i, d[i]);
+    }
+
+    for (int i : n) {
+      System.out.printf("Nilai dari integer forEach, value: %d\n", i);
+    }
+
+    for (float i : f) {
+      System.out.printf("Nilai dari float forEach, value: %f\n", i);
+    }
+
+    for (double i : d) {
+      System.out.printf("Nilai dari double forEach, value: %f\n", i);
+    }
+  }
 }
